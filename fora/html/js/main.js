d5a82a755c95dc63cd0292a162a72ca04b9711ab
seanwu99 <seanwu99@gmail.com>
2018-10-31 16:45:56 -0400

    // init

---
+++ fora/html/js/main.js
@@ -0,0 +1,315 @@
+$(function($) {
+
+  "use strict";
+
+
+/*=========================== scroll background ===========================*/
+
+  $(window).scroll(function(){
+
+    var wScroll = $(this).scrollTop();
+
+    // Activate menu
+    if (wScroll >0) {
+      $('.navbar').addClass('active_sc');
+    }
+    else {
+      $('.navbar').removeClass('active_sc');
+    };
+        
+  });
+
+  /*=========================== close scroll background ===========================*/
+
+ 
+
+
+
+  /*=========================== preloader ===========================*/
+  // Wait for window load
+  $(window).on('load', function() {
+     $(".se-pre-con").fadeOut("slow");;
+  });
+
+  /*=========================== preloader ===========================*/
+
+
+
+  /*=========================== feature owl ===========================*/
+  var owls = $("#feature-owl");
+    owls.owlCarousel({
+
+        autoplay: true,
+        autoplayTimeout:6000,
+        autoplayHoverPause:true,
+        items: 3,
+        loop: true,
+        center: false,
+        margin: 20,
+        stagePadding: 0,
+        dots:true,
+        nav:false,
+
+        animateOut:'fadeOutDown',
+        animateIn:'fadeInDown',
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            768: {
+                items: 3,
+                nav: false,
+                loop: true
+            }
+        }
+    });
+  /*=========================== feature owl ===========================*/
+
+
+//counter js
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+
+ /* ------------------------------- AOS Animation */
+        AOS.init({
+          duration: 1000,
+          mirror: true
+        });
+
+ /*=========================== isotop active ===========================*/
+
+// Isotope for portfolio
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: 1
+  }
+});
+
+
+
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.filter-button-group').each(function(i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
+});
+
+
+/*=========================== isotop active ===========================*/
+
+
+ /*=========================== popup image ===========================*/
+
+//popup gallery
+  $('.popup-gallery').magnificPopup({
+      delegate: '.popup-link',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      }
+  });
+/*=========================== popup image ===========================*/
+
+
+
+
+});
+
+
+/*custom slect*/
+
+$('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+        //console.log($this.val());
+    });
+
+});
+
+
+
+/*add maps*/
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+
+
+
+
+
+/*counter plus minus*/
+//plugin bootstrap minus and plus
+//http://jsfiddle.net/laelitenetwork/puJ6G/
+$('.btn-number').click(function(e){
+    e.preventDefault();
+    
+    fieldName = $(this).attr('data-field');
+    type      = $(this).attr('data-type');
+    var input = $("input[name='"+fieldName+"']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+        if(type == 'minus') {
+            
+            if(currentVal > input.attr('min')) {
+                input.val(currentVal - 1).change();
+            } 
+            if(parseInt(input.val()) == input.attr('min')) {
+                $(this).attr('disabled', true);
+            }
+
+        } else if(type == 'plus') {
+
+            if(currentVal < input.attr('max')) {
+                input.val(currentVal + 1).change();
+            }
+            if(parseInt(input.val()) == input.attr('max')) {
+                $(this).attr('disabled', true);
+            }
+
+        }
+    } else {
+        input.val(0);
+    }
+});
+$('.input-number').focusin(function(){
+   $(this).data('oldValue', $(this).val());
+});
+$('.input-number').change(function() {
+    
+    minValue =  parseInt($(this).attr('min'));
+    maxValue =  parseInt($(this).attr('max'));
+    valueCurrent = parseInt($(this).val());
+    
+    name = $(this).attr('name');
+    if(valueCurrent >= minValue) {
+        $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the minimum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+    if(valueCurrent <= maxValue) {
+        $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+    } else {
+        alert('Sorry, the maximum value was reached');
+        $(this).val($(this).data('oldValue'));
+    }
+    
+    
+});
+$(".input-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+             // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+
+
+/*coming soon page*/
+
+(function () {
+  // Specify the deadline date
+  var deadlineDate = new Date('Dec 31, 2018 23:59:59').getTime();
+
+  // Cache all countdown boxes into consts
+  var countdownDays = document.querySelector('.countdown__days .number');
+  var countdownHours = document.querySelector('.countdown__hours .number');
+  var countdownMinutes = document.querySelector('.countdown__minutes .number');
+  var countdownSeconds = document.querySelector('.countdown__seconds .number');
+
+  // Update the count down every 1 second (1000 milliseconds)
+  setInterval(function () {
+    // Get current date and time
+    var currentDate = new Date().getTime();
+
+    // Calculate the distance between current date and time and the deadline date and time
+    var distance = deadlineDate - currentDate;
+
+    // Calculations the data for remaining days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+    // Insert the result data into individual countdown boxes
+    countdownDays.innerHTML = days;
+    countdownHours.innerHTML = hours;
+    countdownMinutes.innerHTML = minutes;
+    countdownSeconds.innerHTML = seconds;
+  }, 1000);
+})();
